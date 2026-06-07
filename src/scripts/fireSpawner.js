c0132228e8b38d3ec9c78cdcd57918a4be42cd9e
Paul <flumpstudios@gmail.com>
2026-06-07 01:25:39 +0100

    level 2-6 updates

--- src/scripts/fireSpawner.js
+++ src/scripts/fireSpawner.js
@@ -7,16 +7,19 @@ export default class fireSpawner extends globalThis.ISpriteInstance {
   #moveSpeed = 0;
   #onTime = 0;
   #offTime = 0;
+  #switchTimer = 0;
 
   constructor() {
     super();
-    this.#spawnInterval = this.instVars.spawnInterval;   
+    this.#spawnInterval = this.instVars.spawnInterval;
     this.#animationSpeed = this.instVars.fireAnimationSpeed;
     this.#moveSpeed = this.instVars.moveSpeed;
     this.#onTime = this.instVars.onTime;
     this.#offTime = this.instVars.offTime;
-    
     this.isVisible = false;
+    if (this.instVars.invertTimer) {
+      this.#switchTimer = this.#onTime;
+    }
   }
 
   update = (runtime) => {
@@ -25,6 +28,16 @@ export default class fireSpawner extends globalThis.ISpriteInstance {
     }
 
     this.#spawnTimer += runtime.dt * 60;
+    this.#switchTimer += runtime.dt * 60;
+
+    if (this.#switchTimer > this.#onTime + this.#offTime) {
+      this.#switchTimer = 0;
+    }
+
+    if (this.#switchTimer > this.#onTime) {
+      return;
+    }
+
     if (this.#spawnTimer > this.#spawnInterval) {
       this.#spawnTimer = 0;
       const fire = runtime.objects.Explosion.createInstance(
@@ -37,8 +50,7 @@ export default class fireSpawner extends globalThis.ISpriteInstance {
       fire.behaviors.Bullet.angleOfMotion = this.angle;
       fire.behaviors.Bullet.speed = this.#moveSpeed;
       fire.animationSpeed = this.#animationSpeed;
-
-
+      fire.moveToBottom();
     }
   };
 }
